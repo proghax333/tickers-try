f3e475d97beef7e84b6d07c8a2591f4d9eecee71
proghax333 <skillzworldtech@gmail.com>
2024-02-01 22:41:49 +0530

    clean: ignore build files

--- apps/api/build/database/sequelize.service.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SequelizeService = void 0;
-class SequelizeService {
-    constructor() { }
-}
-exports.SequelizeService = SequelizeService;
